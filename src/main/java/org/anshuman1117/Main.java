daf8723e7881fec099b6575df5076ebaa7c5a37c
Anshuman Singh <anshuman17025@iiitd.ac.in>
2023-02-07 19:18:22 +0530

    Change workspace properties

--- src/main/java/org/anshuman1117/Main.java
+++ src/main/java/org/anshuman1117/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package org.anshuman1117;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("init commit!");
     }
 }
